c6d540c2c829f0fd523bf7964c1175a677440af3
Fatih Uysal <fatihhuysal5252@gmail.com>
2023-01-27 22:21:35 +0100

    Java Ders Notlari

---
+++ src/Ders22_ImmutableClasses/C03_StringEquals.java
@@ -0,0 +1,49 @@
+package Ders22_ImmutableClasses;
+
+public class C03_StringEquals {
+    public static void main(String[] args) {
+
+        // Java her seferinde String lerde yeni obje olusturmamak icin String havuzunu kullanir
+        // Eger String olusturulken bugune kadar yaptigimiz sekilde olusturulursa havuzu kontrol eder
+        // ve ayni varsa bunlari ozlestirir.
+        // Ancak new kullanilir veya bir islem sonucunda String elde edilecek olursa havuza bakmaz
+        // once objeyi olusturur sonra atama yapar
+
+            String str1 = "Java";
+            String str2 = "Java";
+
+            String str3 = new String("Java");
+
+            String a = "Ja";
+            String b = "va";
+
+            String str4 = a+b;
+            String str5 = a.concat(b);
+            String str6 = str5 + "";
+
+        System.out.println(str1.equals(str2)); // true
+        System.out.println(str1.equals(str3)); // true
+        System.out.println(str1.equals(str4)); // true
+        System.out.println(str1.equals(str5)); // true
+        System.out.println(str1.equals(str6)); // true
+        System.out.println(str1.equals("Java")); // true
+
+        // sadece degere bakar equals
+
+        System.out.println("==========");
+
+        System.out.println(str1==str2); // true
+        System.out.println(str1==str3); // false
+        System.out.println(str1==str4); // false
+        System.out.println(str1==str5); // false
+        System.out.println(str1==str6); // false
+        System.out.println(str1=="Java"); // true
+
+        // == hem referansa hem de degere bakar
+
+
+
+
+
+    }
+}
---
+++ src/Ders22_ImmutableClasses/C04_LocalDate.java
@@ -0,0 +1,41 @@
+package Ders22_ImmutableClasses;
+
+import java.time.LocalDate;
+
+public class C04_LocalDate {
+    public static void main(String[] args) {
+
+        LocalDate tarih = LocalDate.now();
+        System.out.println(tarih); // 2023-01-27
+
+        System.out.println(tarih.getDayOfWeek()); // FRIDAY
+        System.out.println(tarih.getMonthValue()); // 1
+        System.out.println(tarih.getMonth()); // JANUARY
+        System.out.println(tarih.getDayOfYear()); // 27
+        System.out.println(tarih.getDayOfMonth()); // 27
+
+        System.out.println(tarih.withYear(2015)); // 2015-01-27
+        System.out.println(tarih.withDayOfMonth(15)); // 2023-01-15
+        System.out.println(tarih.withDayOfMonth(15).withYear(2015)); // 2015-01-15
+
+        System.out.println(tarih.isLeapYear()); // false
+
+        System.out.println(tarih.minusWeeks(5)); // 2022-12-23
+        System.out.println(tarih.minusYears(3).minusMonths(3).minusDays(3));//
+
+        System.out.println(tarih.plusWeeks(19)); // 2023-06-09
+        System.out.println(tarih.plusYears(12)); // 2035-01-27
+
+        LocalDate tarih1 = LocalDate.of(1975,10,13);
+        LocalDate tarih2 = LocalDate.of(1975, 10, 20);
+
+        System.out.println(tarih1.isBefore(tarih2) ? "Tarih1 daha eski" : "Tarih2 daha eski"); // Tarih1 daha eski
+
+        System.out.println(tarih1.isLeapYear()); // false
+
+        System.out.println(tarih.hashCode()); // 4143195
+        System.out.println(tarih1.hashCode()); // 4045453
+        System.out.println(tarih2.minusDays(7).hashCode()); // 4045453
+
+    }
+}
