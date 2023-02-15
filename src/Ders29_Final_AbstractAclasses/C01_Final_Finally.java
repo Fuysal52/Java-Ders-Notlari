cb0fe0882f6f9521bce1d77fc912bf4c0fc211fe
Fatih Uysal <fatihhuysal5252@gmail.com>
2023-02-15 12:39:39 +0100

    Java Ders Notlari

---
+++ src/Ders29_Final_AbstractAclasses/C01_Final_Finally.java
@@ -0,0 +1,15 @@
+package Ders29_Final_AbstractAclasses;
+
+public class C01_Final_Finally {
+
+    final int SAYI = 10;
+    final static String OKUL_ISMI = "Yildiz koleji"; // herkes ulasabilir(static) ve degistiremez. (final)
+    public static void main(String[] args) {
+
+        // final olarak tanimlanan variable'lara sonradan deger atanamaz.
+        // bir convention olarak final variable isimleri buyuk harfle yazilir
+
+
+
+    }
+}
